a98a2986b1ca5ad86bb9ac24150b0901c14d9ae9
Kritartha Adhikari <kritarthadybala10@gmail.com>
2026-03-10 14:35:42 +0545

    pointer

--- udf3.c
+++ udf3.c
@@ -6,7 +6,7 @@ int main(){
 }
 void findSum(){
      int P,Q,start,end;
-    printf("Enter start and end numbers and :\n");
+    printf("Enter start and end numbers:\n");
     scanf("%d%d",&start,&end);
     printf("Enter P and Q:\n");
     scanf("%d%d",&P,&Q);
