0d686c184c504d4ca00b6897d9ef407ae82d24d2
Kritartha Adhikari <kritarthadybala10@gmail.com>
2026-04-04 19:10:26 +0545

    Pointer and structure

--- pointerstruct.c
+++ pointerstruct.c
@@ -1,1 +1,27 @@
 #include <stdio.h>
+struct student{
+    char name[30];
+    int rollNo;
+    float marks;
+};
+int main(){
+    int n,i;
+    printf("Enter the number of students:");
+    scanf("%d",&n);
+    struct student s[n];
+    struct student *sp= s;
+    printf("Enter the records of the students:\n");
+    for(i=0;i<n;i++){
+        printf("Enter the name:");
+                fflush(stdin);
+        scanf("%[^\n]s",(sp+i)->name);
+        printf("Enter the roll number:");
+        scanf("%d",&(sp+i)->rollNo);
+        printf("Enter the marks");
+        scanf("%f",&(sp+i)->marks);
+    }
+    for(i=0;i<n;i++){
+        printf("Student-%d\nName:%s\nMarks:%.2f\n",(sp+i)->rollNo,(sp+i)->name,(sp+i)->marks);
+    }
+    return 0;
+}
